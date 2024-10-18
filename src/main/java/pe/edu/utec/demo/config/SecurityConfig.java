0c6afbfaff404fe15f68535aebb19a9ee4118a1a
mateonoel2 <mateo.noel@utec.edu.pe>
2024-10-18 08:36:40 -0500

    backend lab9

--- src/main/java/pe/edu/utec/demo/config/SecurityConfig.java
+++ src/main/java/pe/edu/utec/demo/config/SecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pe.edu.utec.demo.domain.UserDetailsServiceImpl;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -80,5 +82,17 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Disable Cors Origin to React frontend
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
